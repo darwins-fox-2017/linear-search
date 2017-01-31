cf8f1a55f198633acccd5ed6efcb1aab549268f5
Diky Arga <dikyarga.id@gmail.com>
2017-01-31 15:16:44 +0700

    Version 0 and 1 relesed at a time

--- linear_search.js
+++ linear_search.js
@@ -3,7 +3,14 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
+  var countMatch = 0;
   //write your code here
+  for (var i = 0; i < values.length; i++) {
+    if (values[i] == target) {
+      countMatch += 1
+    }
+  }
+  return countMatch > 0 ? countMatch : -1
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -17,6 +24,13 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  var res = []
+  for (var i = 0; i < values.length; i++) {
+    if (values[i] == target) {
+      res.push(i)
+    }
+  }
+  return res.length > 0 ? res : -1
 }
 
 let banana_arr = "banana".split("");
