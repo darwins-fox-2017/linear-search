a40056ba9e907b5ceb2c756b09e2dad5b6cc8375
mfpebrizal <ftm.pebrizal@gmail.com>
2017-01-31 15:45:59 +0700

    Done all release

--- linear_search.js
+++ linear_search.js
@@ -2,9 +2,24 @@
 
 // Release 0
 console.log("Linear Search")
-let linearSearch = (target, values) => {
+const linearSearch = (target, values) => {
   //write your code here
+  let flag = false
+  let i = 0
+  while(!flag){
+    if(target == values[i]){
+      return values.indexOf(values[i])
+      flag = true
+    }else{
+      i++
+      if(i>values.length){
+        return -1
+      }
+    }
+  }
 }
+
+
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
 
@@ -15,8 +30,17 @@ console.log(linearSearch(9, random_numbers));
 
 // Release 1
 console.log("Global Linear Search")
-let globalLinearSearch = (target, values) => {
+const globalLinearSearch = (target, values) => {
   //write your code here
+  let i = 0
+  let result = []
+  while(i<values.length){
+    if(target === values[i]){
+      result.push(i)
+    }
+    i++
+  }
+return result
 }
 
 let banana_arr = "banana".split("");
