3efc5ec4a2e523cf555c36f4a971c558e973592a
Eri Irawan <eryirawan91@gmail.com>
2017-01-31 15:16:44 +0700

    release 0 done

--- linear_search.js
+++ linear_search.js
@@ -4,6 +4,11 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  for (let i = 0; i < values.length; i++) {
+    while (values.indexOf(target) == -1 || values.indexOf(target) >= 0) {
+      return values.indexOf(target)
+    }
+  }
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
