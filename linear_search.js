819dcd8f636556bd7cc05e337a8ef1dc54d4ef91
hepiska <ego1403@gmail.com>
2017-01-31 15:38:44 +0700

    linier search done

--- linear_search.js
+++ linear_search.js
@@ -3,7 +3,14 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
+  var index=-1;
+  for (var i = 0; i < values.length; i++) {
+    if (target==values[i]) {
+      index=i;
+      break;
+    }
+  }
+  return index;
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -16,10 +23,23 @@ console.log(linearSearch(9, random_numbers));
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+  var index=[];
+  for (var i = 0; i < values.length; i++) {
+    if (target==values[i]) {
+       index.push(i);
+    }
+  }
+
+  if (index.length!=0) {
+    return index;
+  }else{
+    return -1;
+  }
+
 }
 
 let banana_arr = "banana".split("");
 // ["b", "a", "n", "a", "n", "a"]
 console.log(globalLinearSearch("a", banana_arr));
 // [ 1, 3, 5 ]
+console.log(globalLinearSearch("z", banana_arr));
